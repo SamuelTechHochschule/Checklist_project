fceebbdf43302ba1e59aa27a7739036ccc8c5caf
SamuelTechHochschule <s210939@th-ab.de>
2023-11-09 14:04:03 +0100

    changes to database test

--- api/db.cjs
+++ api/db.cjs
@@ -1,12 +1,13 @@
 const pgp = require('pg-promise')();
+const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD} = process.env;
 
 //Variablen von process.env 
 const db = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
 });
 
 //Verbindungstest
@@ -16,7 +17,7 @@ db.connect()
         console.log('Datenbankverbindung erfolgreich hergestellt');
     })
     .catch(error => {
-        console.error('Fehler bei der Datenbankverbindung:', error.message);
+        console.error('Fehler bei der Datenbankverbindung:', error.message, process.env.DB);
         process.exit(1);
     });
 
