09bb02c83b335a7c18c12dd5fd5f3791ea9b53d1
SamuelTechHochschule <s210939@th-ab.de>
2023-11-09 16:14:29 +0100

    Test zur Datenbankverbindung

---
+++ test/db.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const db = require('../server/db');
+
+describe('Datenbankverbindung', () => {
+    it('Sollte eine erfolgreiche Verbindung zur Datenbank herstellen', async () => {
+        const connection = await db.connect();
+        expect(connection).to.be.an('object');
+        connection.done();
+    });
+
+    it('Sollte auf einen Fehler bei ungültigen Verbindungsdaten reagieren', async () => {
+        process.env.DB_USER = 'Unknown';
+        process.env.DB_PASSWORD = 'Wrong_password';
+
+        await expect(db.connect()).to.be.rejected;
+
+        process.env.DB_USER = 'postgres';
+        process.env.DB_PASSWORD = 'postgres';
+    });
+});
