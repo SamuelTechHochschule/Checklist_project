5dc8a8b49db1bf94a5dfaeffa0acc095e2b5372d
SamuelTechHochschule <s210939@th-ab.de>
2023-11-09 11:45:03 +0100

    Fehlerbehandlung in database connection

--- api/db.js
+++ api/db.js
@@ -1,5 +1,6 @@
 const pgp = require('pg-promise')();
 
+//Variablen von process.env 
 const db = pgp({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -7,5 +8,16 @@ const db = pgp({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
 });
+
+//Verbindungstest
+db.connect()
+    .then(obj => {
+        obj.done();
+        console.log('Datenbankverbindung erfolgreich hergestellt');
+    })
+    .catch(error => {
+        console.error('Fehler bei der Datenbankverbindung:', error.message);
+        process.exit(1);
+    });
 
 module.exports = db;
