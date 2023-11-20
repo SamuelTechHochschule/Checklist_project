f829bbde2250b56353c6afc418cf470f0c978bab
SamuelTechHochschule <s210939@th-ab.de>
2023-11-20 08:13:44 +0100

    Added Version image

--- pages/Checklist.vue
+++ pages/Checklist.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="navbar">
         <img src="~/assets/Logo.png" alt="Logo" class="logo">
+        <div class="version-block">
+            <img src="~/assets/Version.png" alt="Version" class="version">
+            <p>Version</p>
+        </div>
     </div>
 </template>
 
@@ -16,6 +20,12 @@ export default {
         max-height: 100%;
         width: 125px;
         height: 125px;
+    }
+    .version{
+        max-width: 100%;
+        max-height: 100%;
+        width: 50px;
+        height: 70px;
     }
     
 </style>
