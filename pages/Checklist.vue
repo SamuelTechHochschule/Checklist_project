42c4a21d78bbadf94509b042e331f20e9ad9cc28
SamuelTechHochschule <s210939@th-ab.de>
2023-11-20 08:31:00 +0100

    Added filter and view

--- pages/Checklist.vue
+++ pages/Checklist.vue
@@ -1,10 +1,26 @@
 <template>
     <div class="navbar">
         <img src="~/assets/Logo.png" alt="Logo" class="logo">
-        <div class="version-block">
-            <img src="~/assets/Version.png" alt="Version" class="version">
-            <p>Version</p>
-        </div>
+        <ul>
+            <li>
+                <div class="version-block" href="#">
+                    <img src="~/assets/Version.png" alt="Version" class="version">
+                    <p>Version</p>
+                </div>
+            </li>
+            <li>
+                <div class="view-block">
+                    <img src="~/assets/View.png" alt="View" class="view">
+                    <p>Ansicht</p>
+                </div>
+            </li>
+            <li>
+                <div class="filter-block">
+                    <img src="~/assets/Filter.png" alt="Filter" class="filter">
+                    <p>Filter</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -26,6 +42,21 @@ export default {
         max-height: 100%;
         width: 50px;
         height: 70px;
+    }
+    .version-block{
+        color: #00315E;
+    }
+    .view{
+        max-width: 100%;
+        max-height: 100%;
+        width: 75px;
+        height: 70px;
+    }
+    .filter{
+        max-width: 100%;
+        max-height: 100%;
+        width: 60px;
+        height: 55px; 
     }
     
 </style>
