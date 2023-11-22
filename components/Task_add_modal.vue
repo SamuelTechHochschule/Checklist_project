33932a6721ee820d250400d843f07c7a108c9b16
SamuelTechHochschule <s210939@th-ab.de>
2023-11-22 12:45:20 +0100

    Add Task_add_modal-Komponente

---
+++ components/Task_add_modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="modal" v-if="isOpen">
+        <div class="modal-content">
+            <span class="close" @click="closeModal">$times;</span> 
+            <h3>Task hinzufügen</h3>
+            <label>Aufgabenbeschreibung:</label>
+            <input v-model="newTask.task" type="text" />
+            <label>Abteilung:</label>
+            <input v-model="newTask.department" type="text" />
+            <label>Person:</label>
+            <input v-model="newTask.person" type="text" />
+            <label>Termin geplant:</label>
+            <input v-model="newTask.plannedDate" type="text" />
+            <label>
+                Preliminary Version
+                <input v-model="newTask.isPreliminary" type="checkbox" />
+            </label>
+            <label>
+                Release Version
+                <input v-model="newTask.isRelease" type="checkbox" />
+            </label>
+            <button @click="addTask">Bestätigen</button>
+            <button @click="closeAddTaskModal">Abbrechen</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/Checklist.vue
+++ pages/Checklist.vue
@@ -65,32 +65,6 @@
                 </table>
             </div>
         </div>
-        <div>
-            <div v-if="isAddTaskModalOpen" class="modal">
-                <div class="modal-content">
-                    <span class="close" @click="closeAddTaskModal">$times;</span> 
-                    <h3>Task hinzufügen</h3>
-                    <label>Aufgabenbeschreibung:</label>
-                    <input v-model="newTask.task" type="text" />
-                    <label>Abteilung:</label>
-                    <input v-model="newTask.department" type="text" />
-                    <label>Person:</label>
-                    <input v-model="newTask.person" type="text" />
-                    <label>Termin geplant:</label>
-                    <input v-model="newTask.plannedDate" type="text" />
-                    <label>
-                        Preliminary Version
-                        <input v-model="newTask.isPreliminary" type="checkbox" />
-                    </label>
-                    <label>
-                        Release Version
-                        <input v-model="newTask.isRelease" type="checkbox" />
-                    </label>
-                    <button @click="addTask">Bestätigen</button>
-                    <button @click="closeAddTaskModal">Abbrechen</button>
-                </div>
-            </div>
-        </div>
 </template>
 
 <script>
