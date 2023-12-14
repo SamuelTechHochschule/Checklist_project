805271f61a510301d917cbbec744ba36e3c33594
SamuelTechHochschule <s210939@th-ab.de>
2023-12-14 10:31:01 +0100

    loginToken for mitarbeiterliste

--- pages/Mitarbeiterliste.vue
+++ pages/Mitarbeiterliste.vue
@@ -5,9 +5,24 @@
 </template>
 
 <script>
-import Taskbar from '~/components/Taskbar.vue'
+import Taskbar from '~/components/Taskbar.vue';
+import { useAuthStore } from '~/store/authentication';
 
 export default {
+
+    beforeRouteEnter(to, from, next) {
+        const authStore = useAuthStore();
+
+        // Überprüfen, ob der Benutzer authenifiziert ist
+        if(!authStore.isLoggedIn) {
+            // User ist nicht angemeldet, zurück in Login-Seite leiten
+            next({ path: '/' });
+
+        } else {
+            // Benutzer ist angemeldet, Zugriff erlauben
+            next();
+        }
+    },
 
     components: {
         Taskbar,
