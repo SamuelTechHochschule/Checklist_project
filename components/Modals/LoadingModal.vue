204211d7ef9e1e959ef422d9d68218dfc2826c2b
SamuelTechHochschule <s210939@th-ab.de>
2024-01-19 09:26:57 +0100

    loading indicator in extra component

---
+++ components/Modals/LoadingModal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-if="isLoading" class="modal">
+        <svg class="loading-indicator"></svg>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isLoading: {
+            type: Boolean,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+    .modal{
+        position: fixed;
+        z-index: 9999;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgba(131, 131, 131, 0.4);
+    }
+
+    .loading-indicator{
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50px;
+        height: 50px;
+        border: 5px solid #3498db;
+        border-top: 5px solid transparent;
+        border-radius: 50%;
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin{
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
--- pages/Checklist.vue
+++ pages/Checklist.vue
@@ -1,8 +1,6 @@
 <template>
 
-    <div v-if="isLoading" class="modal">
-            <svg class="loading-indicator"></svg>
-    </div>
+    <LoadingModal :is-loading="isLoading" />
 
     <div class="fixed_column">
 
@@ -52,6 +50,7 @@
 import AddTaskModal from '~/components/Modals/AddTaskModal.vue';
 import EditTaskModal from '~/components/Modals/EditTaskModal.vue';
 import VersionModal from '~/components/Modals/VersionModal.vue';
+import LoadingModal from '~/components/Modals/LoadingModal.vue';
 import ChecklistTable from '~/components/ChecklistTable.vue';
 import Taskbar from '~/components/Taskbar.vue';
 import CalenderView from '~/components/CalenderView.vue';
@@ -80,6 +79,7 @@ export default {
         EditTaskModal,
         Taskbar,
         VersionModal,
+        LoadingModal,
     },
 
     data() {
@@ -386,35 +386,6 @@ export default {
 </script>
 
 <style scoped>
-
-    .modal{
-        position: fixed;
-        z-index: 9999;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgba(131, 131, 131, 0.4);
-    }
-
-    .loading-indicator{
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 50px;
-        height: 50px;
-        border: 5px solid #3498db;
-        border-top: 5px solid transparent;
-        border-radius: 50%;
-        animation: spin 1s linear infinite;
-    }
-
-    @keyframes spin{
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
-    }
 
     .calendar-container{
         flex-grow: 1;
